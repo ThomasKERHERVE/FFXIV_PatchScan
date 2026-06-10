7b4df611aa386ba8930f7dd66b47497b45b81848
ThomasKERHERVE <titom.kerherve@gmail.com>
2026-06-10 11:15:34 +0200

    Modification design

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,11 +5,14 @@ import GlamourCard from "./GlamourCard";
 import "./Dashboard.css";
 
 export default function Dashboard({ data }) {
+
+  const cleanTitle = data.patch_title.split(" | ")[0];
+
   return (
     <div className="dashboard">
       <div className="patch-meta">
         <div>
-          <h2 className="patch-title">{data.patch_title || "Patch Notes"}</h2>
+          <h2 className="patch-title">{cleanTitle || "Patch Notes"}</h2>
           {data.patch_date && <span className="patch-date">{data.patch_date}</span>}
         </div>
         {data.patch_url && (
